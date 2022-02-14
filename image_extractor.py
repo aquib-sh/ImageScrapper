69ef91c9c74f16b4e524c4868ebe274d16171f7d
aquib-sh <shaikhaquib394@gmail.com>
2022-02-14 19:20:10 +0530

    Improve documentation and simplify code.

--- image_extractor.py
+++ image_extractor.py
@@ -1,117 +1,161 @@
-import requests
-from bs4 import BeautifulSoup
+#   IMAGE EXTRACTOR
+# A simple tool for searching and downloading images from bing search
+# use Python version > 3.7
+
 import os
 import random
 import time
+from string import ascii_letters
 
+import requests
+from bs4 import BeautifulSoup
 
-#generating random sequence to name the extracted images
-def generateRandomSequence():
-    seq = ""
-    letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
-               "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
-                ]
-    for i in range(0,5):
-        seq = seq + random.choice(letters) + str(random.randrange(1,1000))
+def generate_random_sequence() -> str:
+    """Generates a sequence of random letters and numbers,
+    to be used in naming images later.
+
+    Returns
+    --------
+    name: str
+        A name made up of 10 letters and 5 numbers randomly.
+        example: "RbCsfpoxqM8686241812"
+
+    """
+    name = ""
+    # Add 10 random letters to the string
+    for i in range(0, 10):
+        letter = random.choice(ascii_letters)
+        name += letter
+
+    # Add 5 random numbers to the string
+    for i in range(0, 5):
+        number = random.randint(0, 100)
+        name += str(number) # str() is used to typecast number to a string
+
+    return name
+
+def write_image_from_link(link, output_folder="Results") -> int:
+    """Downloads the image from link and writes it to a JPEG file 
+    having random name generated from generate_random_sequence()
+
+    Parameters
+    ----------
+    link: str
+        URL of the image
+        example: "https://media.sciencephoto.com/image/e1300006/800wm/E1300006-Avalanche.jpg".
+
+    output_folder: str (DEFAULT="Results")
+        output folder where images will be downloaded to
+        example: Results
+
+    Returns
+    ------
+    :int
+        0 if the download and write operation is sucessful
+        1 if the download and write operation failed
+
+    """
+    # if the folder doesn't exists then create it. 
+    if not os.path.isdir(output_folder):
+        os.mkdir(output_folder)
+        print(f"[+] Creating directory {output_folder}")
+
+    # generate a random name for creating file and add .jpeg to that name
+    img_f_name = generate_random_sequence() + ".jpeg"
+    img_path = os.path.join(output_folder, img_f_name)
+
+    # get image data by GET request
+    response = requests.get(link)
+
+    # exit if the download failed.
+    if (response.status_code) != 200:
+        print(f"CONNECTION ERROR: Failed to download from {link}")
+        return 1
+
+    image_data = response.content
+
+    # open a file pointer and write the binary data to image
+    fp = open(img_path, "wb")
+    fp.write(image_data)
+    return 0
+
+def extract_links_from_soup(soup) -> list:
+    """Extracts image links from soup.
     
-    return seq
+    Parameters
+    -----------
+    soup: BeautifulSoup
+        BeautifulSoup object from where we will extract links
 
+    Returns
+    -------
+    links: list
+        List of image links.
+    """
+    links = []
+    image_elems = soup.find_all("a", {"class":"iusc"})
+    for elem in image_elems:
+        image_info = eval(elem["m"]) #convert string json key:value data to python dict
+        img_url = image_info["murl"]
+        links.append(img_url)
+    return links
 
-#writing imagees to the result folder present in the file directory
-def write_images(links):
+def intro():
+    print("$"*37)
+    print("$"*10, "IMAGE EXTRACTOR", "$"*10)
+    print("$"*37)
+    print("\nAUTHOR: SHAIKH AQUIB\n")
 
-    print("")
-    choice = int(input("How many images do you want to extract ? : "))
-    print("")
+def outro():
+    print("Bye :)")
 
-    #creating the Result directory if it already doesn't exits
-    dir_name = "Result"
-    if os.path.isdir(dir_name) == False:
-        print("[+] Creating Directory Named '{0}'".format(dir_name))
-        os.mkdir(dir_name)
-        
-   
-    n = 1               #maintinging the counter for user set limit
-    for i in links:
-        try:
-            req = requests.get(i)
-            if(n > choice):         #break if the desired limit has reached
-                break
-            print("[+] Extracting Image #",n)
-            with open(("{0}/" + generateRandomSequence() + ".jpg").format(dir_name),"wb") as img:
-                img.write(req.content)
-            n += 1
-            req.close()
-        except:
-            print("[-] Skipping Current Image Due To Connection Problems")
-            continue
+def main():
+    """Main routine."""
+    # it is necessary to spoof user agent otherwise bing might reject requests
+    USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
+    request_header = {"user-agent":USER_AGENT}
 
-#main scrape function
-def scrape():
-    while True:
+    img_count = 1
+
+    intro()
+
+    # Start the main process
+    while (1):
         print("*** enter q to quit ***")
         term = input("Enter the search term: ")
     
         if term == "q":
+            outro()
             break
-        
-        url = "https://bing.com"
-    
 
-        USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
+        img_count = int(input("How many images do you want? (1-35): "))
 
-        r = requests.get(url + "/images/search", params={"q":term,"safeSearch":"off"},headers = {"user-agent": USER_AGENT})
+        response = requests.get(
+            url = f"https://www.bing.com/images/search?q={term}&safeSearch=off",
+            headers = request_header
+            )
 
-        soup = BeautifulSoup(r.content,"html.parser")
+        # If the request is not sucessful then skip
+        if (response.status_code != 200):
+            print(f"[!] Error: request for {term} returned with {response.status_code}")
+            continue
 
-        #getting the links from the scrapped html code
-        li = soup.find_all("a",class_="iusc")
-        links = [eval(l['m'])['murl'] for l in li]
-        len(links)
+        # parse the HTML source
+        soup = BeautifulSoup(response.content, "html.parser")
+        image_urls = extract_links_from_soup(soup)
+        total_images = len(image_urls)
 
-        
-        print("{0} results found with the search term: {1}".format(len(links), term))
-        choice = input("Do You Want To Extract The Images? Y or N ")
-        
-        if choice == "y" or choice == "Y":
-            write_images(links) #writing images using the previously defined function
+        for i in range(0, img_count):
+            # break if total images have been read from list
+            if (i >= total_images):
+                break
 
-        r.close()
-    
+            url = image_urls[i]
+            print(f"[+] Extracting image #{i+1}")
+            status = write_image_from_link(url)
+            if status == 1:
+                print("[!] Failed")
+            
 
-
-
-
-start = """
-$$$$$$$       $$$     $$$       $$$$$$           $$$$$$$$   $$       $$   $$$$$$$$$$
-  $$         $$ $$   $$ $$      $$               $$           $$    $$        $$
-  $$        $$  $$  $$   $$     $$  $$$$         $$$$$$$$       $$$$          $$
-  $$       $$    $ $$     $$    $$  $$ $         $$           $$    $$        $$
-$$$$$$$   $$               $$   $$$$$$ $         $$$$$$$$   $$       $$       $$
-
-
-COPYRIGHT(C) 2020 SHAIKH AQUIB             
-
-                     """
-print(start)
-print("[+] Starting...")
-print("")
-time.sleep(2)
-
-scrape()
-
-   
-    
-
-
-      
-
-
-
-
-        
-
-
-
-
-
+if __name__ == "__main__":
+    main()
